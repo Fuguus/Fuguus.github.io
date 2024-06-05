cb28e4d5722ba0ce8524213cf34d3e085db9b981
Fuguus <128326450+Fuguus@users.noreply.github.com>
2024-06-05 02:27:48 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to initialize the map
+function initMap() {
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
