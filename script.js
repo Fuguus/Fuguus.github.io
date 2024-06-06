b85793ae9757f1520d4795da84a67e45274813a0
Fuguus <128326450+Fuguus@users.noreply.github.com>
2024-06-05 23:27:12 -0700

    update1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function initMap() {
     var mapOptions = {
         center: { lat: 34.1284, lng: 133.6628 },
-        zoom: 8 // Higher zoom level for closer view
+        zoom: 15 // Higher zoom level for closer view
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
